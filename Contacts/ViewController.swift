d0cb684d8eca0b093835eb9787f6b5452690cdea
byLLIPyT <byllipyt@icloud.com>
2021-06-08 23:04:54 +0300

    + add swipe action

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -39,7 +39,13 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        <#code#>
+        let actionDelete = UIContextualAction(style: .destructive, title: "Delete") { _,_,_ in
+            self.contacts.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+        
+        let actions = UISwipeActionsConfiguration(actions: [actionDelete])
+        return actions
     }
     
     
